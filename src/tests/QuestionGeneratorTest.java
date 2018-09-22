c29b70c39238d450ad64839608c1d2a0ee32bd5a
ninnikukawaii <ninnikukawaii@ya.ru>
2018-09-22 20:44:48 +0500

    Добавление теста

--- src/tests/QuestionGeneratorTest.java
+++ src/tests/QuestionGeneratorTest.java
@@ -1,19 +1,42 @@
 package tests;
 
-import console.Question;
-import console.QuestionGenerator;
-import org.junit.Assert;
+import console.Quiz;
+import service.ItemsGenerator;
+import service.Question;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.*;
 
 public class QuestionGeneratorTest {
 
+    private ItemsGenerator<Question> createGenerator() {
+        ItemsGenerator<Question> generator = null;
+        try {
+            generator = new ItemsGenerator<>(Quiz.parseQuestions("questionsForTesting.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return generator;
+    }
+
     @Test
     public void testGenerator() {
-        QuestionGenerator generator = new QuestionGenerator("questionsForTesting.txt");
+        ItemsGenerator<Question> generator = createGenerator();
+        ArrayList<Question> questions = (ArrayList<Question>) generator.getAllItems();
+        assertThat(questions, hasItem(new Question("Сколько будет 2х2?", "4")));
+    }
 
-        Assert.assertTrue(generator.getAllQuestions().contains(
-                new Question("Сколько будет 2х2?", "4")));
-        Assert.assertFalse(generator.getAllQuestions().contains(
-                new Question("Сколько будет 2х2?", "5")));
+    @Test
+    public void testCountDecreases() {
+        ItemsGenerator<Question> generator = createGenerator();
+        int firstCount = generator.getCountOfItems();
+        generator.iterator().next();
+        int secondCount = generator.getCountOfItems();
+        assertTrue(firstCount > secondCount);
     }
 }
