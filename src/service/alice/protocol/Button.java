69ecae4d48f8b629f3dafcc91f7d2889a469cebd
Sshirleyyy <43279077+Sshirleyyy@users.noreply.github.com>
2018-11-12 20:31:57 +0300

    Update src/service/alice/protocol/Button.java

--- src/service/alice/protocol/Button.java
+++ src/service/alice/protocol/Button.java
@@ -1,5 +1,8 @@
 package service.alice.protocol;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import service.userAnswerProcessing.Command;
 import service.userAnswerProcessing.UserStateType;
 
@@ -7,14 +10,14 @@ import java.util.HashMap;
 
 public class Button {
     private String title;
-    private Request payload;
+    //private String payload;
     private Boolean hide;
 
-    public static final Button showHelp = new Button("Показать справку", Command.Help.getName());
-    public static final Button startQuiz = new Button("Запустить викторину", Command.Quiz.getName());
-    public static final Button exitQuiz = new Button("Завершить викторину", Command.Exit.getName());
-    public static final Button showScore = new Button("Узнать счет", Command.Score.getName());
-    public static final Button exit = new Button("Завершить общение", Command.Exit.getName());
+    public static final Button showHelp = new Button("Справка", Command.Help.getName());
+    public static final Button startQuiz = new Button("Викторина", Command.Quiz.getName());
+    public static final Button exitQuiz = new Button("Выход", Command.Exit.getName());
+    public static final Button showScore = new Button("Счет", Command.Score.getName());
+    public static final Button exit = new Button("Выход", Command.Exit.getName());
 
     public static final HashMap<UserStateType, Button[]> defaultButtons;
     static {
@@ -27,10 +30,19 @@ public class Button {
 
     public String getTitle() { return title; }
 
-    public String getCommand() { return payload.getCommand(); }
+    public String getCommand() {
+        Gson gsonBuilder = new GsonBuilder().setPrettyPrinting().create();
+        //Request reques = gsonBuilder.fromJson(this.payload, Request.class);
+        return this.title;
+    }
 
     private Button(String title, String command){
         this.title = title;
-        this.payload = new Request(command);
+        Gson gson = new Gson();
+        Request payloa = new Request(command);
+        JsonObject jsonObject =  new JsonObject();
+        jsonObject.addProperty("command", command);
+        this.hide = true;
+        //this.payload = jsonObject.toString();
     }
 }
